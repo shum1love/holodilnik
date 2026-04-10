568b3e63725d2963ef41d04af7a2aeb5dafdcac7
Rodion Shumilov <rodya7911@gmal.com>
2026-04-10 17:51:49 +0300

    EmptySearchResultTest complete

--- src/test/java/ru/holodilnik/tests/ui/EmptySearchResultTest.java
+++ src/test/java/ru/holodilnik/tests/ui/EmptySearchResultTest.java
@@ -24,7 +24,6 @@ public class EmptySearchResultTest {
                 .open()
                 .checkMainElements()
                 .inputValueSearchField("asdfghjkl")
-                .checkNothingWasFound()
-        ;
+                .checkNothingWasFound();
     }
 }
